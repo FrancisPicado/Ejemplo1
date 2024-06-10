aa083c03212b4808cb055afce98d0f1a2a65bcaf
Francis Rossebell Picado M <francispicadomairena7299@gmail.com>
2024-06-09 23:15:53 -0600

    Proyecto inicial

---
+++ Ejemplo1/src/main/java/fidelitas/ejemplo1/Ejemplo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fidelitas.ejemplo1;
+
+/**
+ *
+ * @author franc
+ */
+public class Ejemplo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
