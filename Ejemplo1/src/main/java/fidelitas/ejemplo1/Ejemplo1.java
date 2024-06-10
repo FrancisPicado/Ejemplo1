6575ea6f9cdf4b15e61ed396eed6075a4b4516fa
Francis Rossebell Picado M <francispicadomairena7299@gmail.com>
2024-06-09 23:30:28 -0600

    nn

--- Ejemplo1/src/main/java/fidelitas/ejemplo1/Ejemplo1.java
+++ Ejemplo1/src/main/java/fidelitas/ejemplo1/Ejemplo1.java
@@ -16,6 +16,6 @@ public class Ejemplo1 {
         System.out.println("Hello World!");
         JOptionPane.showMessageDialog(null, "hola");
         JOptionPane.showMessageDialog(null, "Buenas");
-        JOptionPane.showMessageDialog(null, "buenas noches");
+        JOptionPane.showMessageDialog(null, "buenas noches brach FRAN");
     }
 }
